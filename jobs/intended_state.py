c91f6da2ee3765c83caa686b727568b36d530614
Joe Wesch <joe.wesch@networktocode.com>
2022-10-26 14:15:58 -0500

    Initial commit

---
+++ jobs/intended_state.py
@@ -0,0 +1,25 @@
+import json
+
+from nautobot.extras.jobs import Job, TextVar
+from nautobot.utilities.utils import get_model_from_name
+
+name = "POC Jobs"
+
+
+class IntendedState(Job):
+
+    json_payload = TextVar()
+
+    class Meta:
+        name = "Intended State Job POC"
+        description = "Create or update objects in Nautobot by passing in an intended state JSON payload."
+
+    def run(self, data, commit):
+        json_payload = data["json_payload"]
+        intended_state = json.loads(json_payload)
+        for object_name, object_data in intended_state.items():
+            object_class = get_model_from_name(object_name)
+            object_class.objects.update_or_create(**object_data)
+
+
+jobs = [IntendedState]
