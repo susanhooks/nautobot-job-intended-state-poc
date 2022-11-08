cd5a145d1cf4fd30a3230191481df1e18e1ad727
Joe Wesch <joe.wesch@networktocode.com>
2022-11-08 08:47:16 -0600

    Adds string reference replace function

--- jobs/intended_state.py
+++ jobs/intended_state.py
@@ -1,9 +1,20 @@
 import json
+import re
+
+from django.apps import apps
 
 from nautobot.extras.jobs import Job, TextVar
-from nautobot.utilities.utils import get_model_from_name
 
 name = "POC Jobs"
+
+
+def replace_ref(ref):
+    pattern = r"^#ref:(?P<app>.*):(?P<field>.*):(?P<value>.*)$"
+    groups = re.match(pattern, ref)
+    app_name = groups.group("app")
+    obj_lookup = {groups.group("field"): groups.group("value")}
+    object_class = apps.get_model(app_name)
+    return object_class.objects.get(**obj_lookup)
 
 
 class IntendedState(Job):
@@ -18,8 +29,11 @@ class IntendedState(Job):
         json_payload = data["json_payload"]
         intended_state = json.loads(json_payload)
         for object_name, objects in intended_state.items():
-            object_class = get_model_from_name(object_name)
+            object_class = apps.get_model(object_name)
             for object_data in objects:
+                for key, value in object_data.items():
+                    if value.startswith("#ref"):
+                        object_data[key] = replace_ref(value)
                 obj, created = object_class.objects.update_or_create(**object_data)
                 self.log_success(obj=obj, message=f"Object {obj} has been {'created' if created else 'updated'}.")
 
